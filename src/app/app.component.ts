cc8746b8e11b084b625f1ab617d37d14c64bf87f
sridhar-web-dev <webdev.sridhar@gmail.com>
2025-03-03 16:39:46 +0530

    2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +6,49 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'learn';
-}
+  islogin = false;
+  orderStatus = 'Completed';
+  products = ['Apple', 'Nokia', 'vivo', 'redmi', 'oppo'];
+  productsWithPrice = [ ['apple', '10'], ['apple', '10']];
+  property = [
+    {
+      'name': 'apple',
+      'price': '100',
+      'year': '1996'
+    },
+    {
+      'name': 'samsung',
+      'price': '80',
+      'year': '2000'
+    },
+    {
+      'name': 'google',
+      'price': '120',
+      'year': '1998'
+    },
+    {
+      'name': 'microsoft',
+      'price': '150',
+      'year': '1975'
+    },
+    {
+      'name': 'amazon',
+      'price': '90',
+      'year': '1994'
+    },
+    {
+      'name': 'tesla',
+      'price': '200',
+      'year': '2003'
+    },
+    {
+      'name': 'sony',
+      'price': '110',
+      'year': '1946'
+    }
+  ];
+  
+
+  textlengh: string = 'Srar';
+username: string = '';
+  }
